3123d4f930981b8e48be4b6f9efb0345419a76ce
CamilaRiveros <cjaviera.rive@gmail.com>
2024-06-17 01:06:13 -0400

    iniciando otra vez

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,48 @@ const API_URL = "https://jsonplaceholder.typicode.com/posts"
 //         postData.innerHTML += `<li><strong>${element.title}</strong><br>${element.body}</li>`;
 //     })
 // }
+
+//construir etiqueta en HTML (document.createElement) para más seguridad
+const displayData = (data) => {
+    data.forEach((element) => {
+        const li = document.createElement("li");
+
+        const title = document.createElement("span");
+        title.textContent = element.title;
+        title.style.fontWeight = "bold";
+        li.appendChild(title);
+
+        const body = document.createElement("p");
+        body.textContent = element.body;
+        li.appendChild(body);
+
+        postData.appendChild(li);
+    });
+};
+
+// solicitud http: con fetch ($.ajax jquery)
+// fetch nos devuelve promesa, resolver con async y await o then
+
+const getData = async () => {
+    try {
+        const response = await fetch(API_URL)
+
+        //validaciones error
+        if (!response.ok) {
+            console.log("la api falló")
+            console.log("código de error: ", response.status)
+        }
+        //procesar el json
+        const data = await response.json()
+        // console.log(data)
+
+        displayData(data)
+
+    } catch (error) {
+        console.log("error", error)
+    }
+
+}
+
+//ejecutar al detectart click (getData sin paréntesis)
+btnPost.addEventListener("click", getData)
